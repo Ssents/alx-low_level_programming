f29b491e9959f8bedbcefca17c047fe45ac21bfb
ssents <ssentumbweenoch@gmail.com>
2022-03-11 13:20:07 -0800

    Add 0 file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	else
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 
 	return (0);
 }
