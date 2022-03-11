d3f8ce75404d43fd629136a4244172c4763ff093
ssents <ssentumbweenoch@gmail.com>
2022-03-11 15:41:16 -0800

    Add 6 code file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - task 2
+ * Description: return alphabet in small case
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
