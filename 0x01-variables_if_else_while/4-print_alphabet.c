e480f9b5e58f06e96df352635afc516aab816326
ssents <ssentumbweenoch@gmail.com>
2022-03-11 15:17:12 -0800

    Add 4 file

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - task 2
+ * Description: return alphabet in small case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
