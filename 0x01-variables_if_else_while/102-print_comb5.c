01426a9ca828298a464a03f3d9bced900d1c4d14
ssents <ssentumbweenoch@gmail.com>
2022-03-12 12:14:04 -0800

    Add 102 file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,19 +15,19 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 9; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					if (i <= j && k <= l)
+					if (l <= k && j <= i)
 					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
 						putchar(l + '0');
+						putchar(k + '0');
+						putchar(' ');
+						putchar(j + '0');
+						putchar(i + '0');
 					}
 
 
