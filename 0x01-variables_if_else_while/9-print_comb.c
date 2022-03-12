df1d2ae769ab03b654dbc24560401897fc3bd1d8
ssents <ssentumbweenoch@gmail.com>
2022-03-11 17:41:42 -0800

    Add 9 file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Task 9
+ * Description: Print single digit combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
