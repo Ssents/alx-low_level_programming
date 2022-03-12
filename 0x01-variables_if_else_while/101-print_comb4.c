28920b6b6e4271b5c3d6af4b722d2c32726c6536
ssents <ssentumbweenoch@gmail.com>
2022-03-12 11:38:37 -0800

    Add 101 code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,7 +24,10 @@ int main(void)
 					putchar(j + '0');
 					putchar(k + '0');
 
-					if (i != 7 && j != 8 && k != 9)
+					if (i == 7 && j == 8 && k == 9)
+					{
+					}
+					else
 					{
 						putchar(',');
 						putchar(' ');
