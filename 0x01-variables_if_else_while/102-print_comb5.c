7ae1ca8aca6035dc7225f90904306b0a8af2f550
ssents <ssentumbweenoch@gmail.com>
2022-03-12 11:59:58 -0800

    Add 102 file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Task 9
+ * Description: Print single digit combinations
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i < j && k < l && i != k && j != l)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+					}
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
