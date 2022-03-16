bf98f32461b82c19bfcfcd48f2896c253d77ed8f
ssents <ssentumbweenoch@gmail.com>
2022-03-16 16:10:19 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - print out the lowercase alphabet 10 times
+ * Description - above hehe
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+	char letter = 'a';
+
+	while ( i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
