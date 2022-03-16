5817d7422746d60edd4779f422938cfaf1c52f1a
ssents <ssentumbweenoch@gmail.com>
2022-03-16 16:49:29 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks if character is of lower case
+ * @c: character input for fucntion
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Task 3
+ * Description - tell if value is lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
