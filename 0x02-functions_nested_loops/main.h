3a5291f4121d3120cd85e1e37a94930bb46f0329
ssents <ssentumbweenoch@gmail.com>
2022-03-15 22:37:45 -0700

    Edit main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * description - custom header file
  * _putchar - puts the character c in the stdout
- * @a - character to be printed
+ * @c - character to be printed
  * Return - on success 1
+ * on error, -1 is returned, and errno is identified
  */
 
-char _putchar (char a)
+char _putchar (char c)
 {
-	return (putchar(a));
+	return (write(1, &c, 1));
 }
