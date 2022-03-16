d77e6370c4fa4941e9f14329a3ad075a04da7f1f
ssents <ssentumbweenoch@gmail.com>
2022-03-16 04:25:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #define _putchar
+
 /**
  * main - Task 0 putchar
  * Description - print _putchar
  * Return: 0
  */
+
 int main(void)
 {
 	char putchar[] = "_putchar";
@@ -13,5 +15,6 @@ int main(void)
 	while (putchar[i] != '\0')
 	{
 		_putchar(putchar[i]);
+		i++;
 	}
 }
