b748a8ae8b6e1dd009f22c633b288bacc497b479
ssents <ssentumbweenoch@gmail.com>
2022-03-16 01:55:18 -0700

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,11 @@ int main(void)
 	char data[] = "_putchar";
 	int dl = sizeof(data);
 	int i;
+	char t = "i";
 
 	for (i = 0; i < dl; i++)
 	{
-		_putchar(data[i]);
+		_putchar(i);
 	}
 	
 	_putchar('\n');
