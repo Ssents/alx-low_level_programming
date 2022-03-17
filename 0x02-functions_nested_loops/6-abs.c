08a319e39ce38b96fa8d762916335650d285091e
ssents <ssentumbweenoch@gmail.com>
2022-03-16 19:21:05 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - return absolute value
+ * @n: character input for fucntion
+ * Return: value
+ */
+int _abs(int n)
+{
+	int value;
+
+	value = n * n / n;
+	return (value);
+}
