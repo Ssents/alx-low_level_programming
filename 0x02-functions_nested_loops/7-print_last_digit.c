8e7a21c7851f262b128dbd7ff59bdd5ea60747b8
ssents <ssentumbweenoch@gmail.com>
2022-03-19 12:46:35 -0700

    Edit 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,6 +4,7 @@
  * _putchar - returna character
  * @n: character input for fucntion
  * ni - converted value
+ * ascii_val -int value for chars
  * value - last digit
  * Return: value
  */
@@ -11,13 +12,22 @@ int print_last_digit(int n)
 {
 	int value;
 	int ni = n;
-	int int_value;
+	int ascii_val;
 
 	if (ni < 0)
 		value = (ni * -1) % 10;
 	else
 		value = ni % 10;
 
-	_putchar((int)value + '0');
+	if (value >= 'A' && value <= 'z')
+	{
+		for (ascii_val = 65; ascii_val <= 122; ascii_val++)
+		{
+			if (ascii_val == ni * 1)
+				value = ascii_val % 10;
+		}
+	}
+
+	_putchar((int)(value + '0'));
 	return (value);
 }
