3bec92551dfab585099297e08b972b0fa7923759
ssents <ssentumbweenoch@gmail.com>
2022-03-19 13:13:19 -0700

    Add 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - returns all minutes in 24 hours
+ * i = int value
+ * j - int value
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		if (i < 10)
+			_putchar('0');
+		_putchar(i + '0');
+		_putchar(':');
+
+		for (j = 0; j < 60; j++)
+		{
+			if (j < 10)
+				_putchar('0');
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
