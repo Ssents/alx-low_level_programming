1858c5adb8ec800b5b2c47b246a2f9a77bccbdf3
ssents <ssentumbweenoch@gmail.com>
2022-03-20 01:23:37 -0700

    Edit 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,6 +4,7 @@
  * @n: value
  * limit: value value
  * new_num: value
+ * neg: value
  * _putchar - print out chars
  */
 
@@ -11,6 +12,7 @@ void print_to_98(int n)
 {
 	int new_num;
 	int limit;
+	int neg;
 
 	if (n > 98)
 	{
@@ -24,25 +26,27 @@ void print_to_98(int n)
 	}
 
 	do {
+		if (new_num < 0)
+		{
+			_putchar('-');
+			neg = -1;
+		}
+		else
+			neg = 1;
 		if (new_num >= 100)
-			_putchar((new_num / 100) + '0');
-
+			_putchar(((new_num * neg) / 100) + '0');
 		if (new_num >= 10)
-			_putchar(((new_num % 100) / 10) + '0');
-
-		_putchar((new_num % 10) + '0');
-
+			_putchar((((new_num * neg) % 100) / 10) + '0');
+		_putchar(((new_num * neg) % 10) + '0');
 		if (n <= 98)
 			new_num++;
 		else
 			new_num = new_num - 1;
-
 		if (new_num != limit)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
-
 	} while (new_num != limit);
 	_putchar('\n');
 }
