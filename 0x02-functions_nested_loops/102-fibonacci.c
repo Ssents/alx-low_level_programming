e7d3dfc30e10981a563b828820da6aa33a3820e5
ssents <ssentumbweenoch@gmail.com>
2022-03-20 08:01:23 -0700

    Add 102 file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print natural numbers below 1024
+ * Description - print first fibonacci numbers
+ * i: count variable
+ * next: next number
+ * prev: number before
+ * sum: sum of prev and next
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int next;
+	int prev;
+	int sum;
+
+	prev = 0;
+	while (i < 50)
+	{
+		sum = prev + next;
+		print("%i", sum);
+		prev = next;
+		next = sum;
+		if (i != 49)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
