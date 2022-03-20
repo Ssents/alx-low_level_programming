a4d9c7ce06d7541d71ea4845631cb49d4d929018
ssents <ssentumbweenoch@gmail.com>
2022-03-20 10:07:40 -0700

    Add 4 file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers below 9;
+ * _putchar - function
+ * i: varibale
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
