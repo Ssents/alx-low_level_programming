5094b919e02fe34991051df9aa74e9cc43327116
ssents <ssentumbweenoch@gmail.com>
2022-03-20 09:25:48 -0700

    Edit 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,16 +15,22 @@ int main(void)
 	long int next;
 	long int prev;
 	long int sum;
+	long int front;
+	long int back;
+	long int max = 10 ^ 19;
 
 	prev = 0;
 	i = 0;
 	next = 1;
 	while (i < 98)
 	{
-		sum = prev + next;
-		printf("%lu", sum);
-		prev = next;
-		next = sum;
+		if ((prev + next < max))
+		{
+			sum = prev + next;
+			printf("%lu", sum);
+			prev = next;
+			next = sum;
+		}
 		if (i != 97)
 			printf(", ");
 		i++;
