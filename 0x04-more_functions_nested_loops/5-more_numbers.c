707da916841a4b562d26dfe6a3dadffa0bd43ccd
ssents <ssentumbweenoch@gmail.com>
2022-03-20 10:16:06 -0700

    Add 5 file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,11 @@ void more_numbers(void)
 	while (i <= 9)
 	{
 		j = 0;
-		while (j <= 9)
+		while (j <= 14)
 		{
-			_putchar(i + '0');
+			if (j > 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			j++;
 		}
 		_putchar('\n');
