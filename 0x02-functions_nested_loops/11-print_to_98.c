b3b6665dd93b4e33b6fa8f93b1e4637282035b89
ssents <ssentumbweenoch@gmail.com>
2022-03-20 00:01:23 -0700

    Add 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: value
+ * limit: value value
+ * new_num: value
+ * _putchar - print out chars
+ */
+
+void print_to_98(int n)
+{
+	int new_num;
+	int limit;
+
+	if (n >= 98)
+	{
+		new_num = 98;
+		limit = 97;
+	}
+	else
+	{
+		limit = 99;
+		new_num = 0;
+	}
+
+	do {
+		if (new_num >= 100)
+			_putchar((new_num / 100) + '0');
+
+		if (new_num >= 10)
+			_putchar(((new_num % 100) / 10) + '0');
+
+		_putchar(new_num + '0');
+		_putchar(',');
+		_putchar(' ');
+
+		if (n < 98)
+			new_num++;
+		else if (n == 98)
+			new_num = 98;
+		else
+			new_num = n - 1;
+
+	} while (new_num != limit);
+	_putchar('\n');
+}
