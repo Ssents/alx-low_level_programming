76386642f41ddc778447aab8048b1fd946b02eae
ssents <ssentumbweenoch@gmail.com>
2022-03-20 07:51:21 -0700

    Edit 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,23 +3,23 @@
  * main - print natural numbers below 1024
  * Description - print all natural multiples of 3 and 5 below 1024
  * i: count variable
+ * sum: total sum of multiples
  * Return: always 0
  */
 
 int main(void)
 {
 	int i;
+	int sum;
 
+	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
 		if (((i % 5) == 0) || ((i % 3) == 0))
 		{
-			printf("%i", i);
-			if (i != 1023)
-				printf(", ");
+			sum = sum + i;
 		}
 	}
-	putchar('\n');
-
+	printf("%i\n", sum);
 	return (0);
 }
