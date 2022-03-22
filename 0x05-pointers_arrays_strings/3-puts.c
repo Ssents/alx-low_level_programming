c10411ef92cb805387d07e95276927e7e2ef7663
ssents <ssentumbweenoch@gmail.com>
2022-03-22 12:45:56 -0700

    Ass task 1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints out a string
+ * _putchar - function that prints chars
+ * @str: string char
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		*str++
+	}
+	_putchar('\n');
+}
