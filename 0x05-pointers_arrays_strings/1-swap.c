566b07c78eab7b733eee65f4419aa2c4972ee1af
ssents <ssentumbweenoch@gmail.com>
2022-03-22 12:33:05 -0700

    Ass task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,15 @@
  * reset_to_98 -function to swap valus
  * @a: input variable
  * @temp: temporary value
+ * temp1: variable
  * @b: another variable
  */
 
 void swap_int(int *a, int *b)
 {
 	int temp = *a;
+	int temp1 = *b;
 
-	*a = b;
+	*a = temp1;
 	*b = temp;
 }
