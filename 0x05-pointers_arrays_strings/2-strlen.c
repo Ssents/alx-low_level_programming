26fa97417ff93ce88307681de4ec4a27ed5c25fb
ssents <ssentumbweenoch@gmail.com>
2022-03-23 03:57:01 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,17 @@
 /**
  * _strlen - returns the length of a string
  * @s: string c
+ * len: int length
  * Return: Length of a string
  */
 int _strlen(char *s)
 {
-	return (sizeof(*s));
+	int len;
+
+	while (*s != '0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
