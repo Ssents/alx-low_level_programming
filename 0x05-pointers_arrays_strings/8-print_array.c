413ba407f8e63601fe1ba26acf7779c78932a06a
ssents <ssentumbweenoch@gmail.com>
2022-03-24 00:30:29 -0700

    Add task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print out only n values of an array
+ * i: increment value
+ * @a: array
+ * @n: limit
+ * len: length of half
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%i", a[i]);
+		if (i < n)
+			printf(", ");
+		i++;
+	}
+	_putchar('\n');
+}
