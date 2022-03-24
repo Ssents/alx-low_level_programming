c3416bf04aca8f6b4d6a2e163897e2bdaac24284
ssents <ssentumbweenoch@gmail.com>
2022-03-23 20:04:52 -0700

    Edit task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,28 +2,28 @@
 /**
  * rev_string - print all string in reversee
  * @s: input varible
- * str: reverse string variable
+ * b: input variable
+ * a: another temporary variable
+ * i: incremental variable
  * _putchar -print one character at atime
  */
 void rev_string(char *s)
 {
 	int length = 0;
 	int i = 0;
+	char b, a;
 
 	while (s[length] != '\0')
 		length++;
 
-	char str[length - 1];
+	while (i < length)
+	{
+		a = s[i];
+		b = s[length];
+		s[i] = a;
+		s[length] = b;
 
-	for (length = length - 1; length >= 0; length--)
-	{
-		str[i] = s[length];
 		i++;
-	}
-	
-	while (s[length] != '\0')
-	{
-		s[length] = str[length];
-		length ++;
+		length--;
 	}
 }
